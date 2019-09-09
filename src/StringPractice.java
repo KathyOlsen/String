4932b4f7344ea4766efb220d9c33d02e33b76ee0
KathyOlsen <katherinekolsen@verizon.net>
2019-09-09 01:04:36 -0400

    Initial commit

---
+++ src/StringPractice.java
@@ -0,0 +1,7 @@
+public class StringPractice {
+    public static void main(String[] args){
+        String name = "Katherine Olsen";
+        String formattedName = String.format("My name is %s", name);
+        System.out.printf(formattedName);
+    }
+}
